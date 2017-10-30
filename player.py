1d260ce294afd56feaf05285d10041a025907956
shbatgh <shboccara@gmail.com>
2017-10-29 21:32:15 -0400

    4th commit

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import random
 import time
+import select
 
 global tmoney
 tmoney = 50000
@@ -14,7 +15,6 @@ first_names = [
 ]
 last_names = ["Xiong", "Lee", "Greene", "Henwich", "Smith"]
 
-
 myteam = ['{} {}'.format(random.choice(first_names), last_name) for last_name in last_names]
 
 print(myteam)
@@ -22,15 +22,15 @@ print(myteam)
 
 
 def run():
+    # function in case i have to rerun my program
     skill = 10 + random.randint(10, 90)
     if __name__ == '__main__':
-        teamy = Team(skill)
-        teamy.make_teams()
-        teamy.bet()
-        run()
+        play_and_bet()
 
 
-def play():
+
+def play_and_bet():
+    # play a game and see if you win or lose
     play_game = input("Write Play to play a match ")
     tmoney = 50000
     mbet = float(input("How much do you want to bet "))
@@ -48,23 +48,22 @@ def play():
         p5 = x + y4
         tskill = p1 + p2 + p3 + p4 + p5
         oskill = 20
-        if tskill > oskill:
-            x = random.randint(1, 10)
-            print(x)
-            if x == 1 or 2:
-                print("You lose")
-                tmoney = tmoney - (mbet * 3)
-                print("You now have " + str(tmoney))
-            if x == 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10:
-                print("You win")
-                tmoney = tmoney + mbet * 5
-                print("you now have " + str(tmoney))
-        elif tskill == oskill:
+        def idk():
+            if tskill > oskill:
+                x = random.randint(1, 10)
+                tmoney = 50000
+                if x == 1 or 2:
+                    print("You lose so you now have " + str(tmoney-mbet*3))
+                    pass
+                if x == 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10:
+                    print("You win so you now have " + str(tmoney+mbet*5))
+                    pass
+        if tskill == oskill:
             x = random.randint(1, 10)
             if x == 1 or 2 or 3 or 4 or 5:
-                print("You win")
-            else:
-                print("You lose")
+                print("You win so you now have " + str(tmoney - mbet*3))
+            elif x == 6 or 7 or 8 or 9 or 10:
+                print("You lose so you now have " + str(tmoney - mbet*2))
 
         if tmoney >= 100000000000000:
             print("You won the game")
@@ -76,44 +75,12 @@ def play():
 
         time.sleep(0.1)
 
-
-
-
 def bet_again():
-    betting = input("Do you want to bet ")
-    if betting == "yes" "Yes":
-        mbet = float(input("How much do you want to bet "))
-        tmoney = 50000
-        tmoney = tmoney - mbet
-    else:
-        print("Are you sure you don't want to bet?")
-        play()
-
-
-class Bet():
-    def __init__(self, tmoney):
-        self.tmoney = tmoney
-
-    def bet(self):
-        tmoney = 50000
-        time.sleep(0.1)
-        betting = input("Do you want to bet ")
-        if betting != "yes" or "Yes":
-            play()
-        elif betting == "yes" or "Yes":
-            global mbet
-            mbet = float(input("How much do you want to bet "))
-            tmoney = 50000
-            tmoney = tmoney - mbet
-
-        mbet = float(input("How much do you want to bet "))
-        if mbet > tmoney:
-            print("You can't bet more than you have")
-            bet_again()
-
-class Team():
-    def __init__(self, skill):
-        self.skill = skill
+    # a function to bet again if something goes wrong with the first bet
+    mbet = float(input("How much do you want to bet "))
+    tmoney = 50000
+    tmoney = tmoney - mbet
+    print(tmoney)
 
 
 class Store():
@@ -138,53 +105,75 @@ class Store():
             print("You have $" + str(tmoney))
         if tmoney < 1000000:
             print("You don't have enough money to buy Micheal Jordan")
-            play()
+            play_and_bet()
         elif buyM != "yes" or "Yes":
-            play()
+            play_and_bet()
 
     def upgrade(self):
-        print(str(myteam))
-        upgrade = input("Type the first letter of your troops to upgrade their skill, which makes you win more ")
-        firstc, firstc1, firstc2, firstc3, firstc4 = person[0][0], person1[0][0], person2[0][0], person3[0][0], person4[0][0]
+        person = myteam[0]
+        person1 = myteam[1]
+        person2 = myteam[2]
+        person3 = myteam[3]
+        person4 = myteam[4]
+        firstc = person[0]
+        firstc1 = person1[0]
+        firstc2 = person2[0]
+        firstc3 = person3[0]
+        firstc4 = person4[0]
         time.sleep(0.3)
         tskill = 50000
-        if upgrade == firstc or firstc1 or firstc2 or firstc3 or firstc4:
-            supgrade = tskill + tskill/250
-            if upgrade == firstc:
-                print("To upgrade " + person + " press u, it will upgrade it's skill will become " + str(supgrade) + "this will cost you $4,500")
-                global u
-                u = input("Are you sure you want spend $4500 to upgrade " + person + " skill to " + str(supgrade + tskill))
-                if u == "u" or "U":
-                    tmoney = tmoney - 4500
-                    tskill = tskill + tskill
-            if upgrade == firstc1:
-                print("To upgrade " + person1 + " press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
-                if u == "u" or "U":
-                    tmoney = tmoney - 4500
-                    tskill = tskill + tskill
-                if upgrade == firstc2:
-                    print("To upgrade " + person2 + "press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
-                if u == "u" or "U":
-                    tmoney = tmoney - 4500
-                    tskill = tskill + tskill
-                if upgrade == firstc3:
-                    print("To upgrade " + person3 + " press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
-                if u == "u" or "U":
-                    tmoney = tmoney - 4500
-                    tskill = tskill + tskill
-            if upgrade == firstc4:
-                print("To upgrade " + person4 + " press u, it will upgrade it's skill will become " + str(supgrade) + "this will cost you $4,500")
-                if u == "u" or "U":
-                    tmoney = tmoney - 4500
-                    tskill = tskill + supgrade
-                    print(tskill)
+        global supgrade
+        supgrade = tskill + tskill / 250
+        global u, u1, u2, u3, u4
+        u = input("To upgrade " + person + " press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
+        u1 = input("To upgrade " + person1 + " press u, it will upgrade it's skill will become " + str(
+            supgrade) + " this will cost you $4,500")
+        u2 = input("To upgrade " + person2 + " press u, it will upgrade it's skill will become " + str(
+            supgrade) + " this will cost you $4,500")
+        u3 = input("To upgrade " + person3 + " press u, it will upgrade it's skill will become " + str(
+            supgrade) + " this will cost you $4,500")
+        u4 = input("To upgrade " + person4 + " press u, it will upgrade it's skill will become " + str(
+            supgrade) + " this will cost you $4,500")
+        print("Are you sure you want spend $4500 to upgrade " + person + " skill to " + str(supgrade + tskill))
+        global tmoney
+        tmoney = 50000
+        if u == firstc:
+            print("To upgrade " + person + " press u, it will upgrade it's skill will become " + str(tskill + supgrade) + "this will cost you $4,500")
+            if u == "u" or "U":
+                tmoney = tmoney - 4500
+                tskill = tskill + supgrade
+                print("Your total skill is now " + str(tskill))
+                pass
+        if u1 == firstc1:
+            print("To upgrade " + person1 + " press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
+            if u == "u" or "U":
+                tmoney = tmoney - 4500
+                tskill = tskill + supgrade
+                print("Your total skill is now " + str(tskill))
+                pass
+        if u2 == firstc2:
+            print("To upgrade " + person2 + "press u, it will upgrade it's skill will become " + str(supgrade) + " this will cost you $4,500")
+            if u == "yes" or "Yes":
+                tmoney = tmoney - 4500
+                tskill = tskill + supgrade
+                print("Your total skill is now " + str(tskill))
+                pass
+        if u3 == firstc3:
+            if u1 == "yes" or "Yes":
+                tmoney = tmoney - 4500
+                tskill = tskill + supgrade
+                print("Your total skill is now " + str(tskill))
+                pass
+        if u4 == firstc4:
+            print("To upgrade " + person4 + " press u, it will upgrade it's skill will become " + str(supgrade) + "this will cost you $4,500")
+            if u == "yes" or "Yes":
+                tmoney = tmoney - 4500
+                tskill = tskill + supgrade
+                print("Your total skill is now " + str(tskill))
+                pass
 skill = 10 + random.randint(10, 90)
-
-
 if __name__ == '__main__':
-    betty = Bet(tmoney)
-    betty.bet()
-    teamy = Team(skill)
+    play_and_bet()
     storey = Store(tmoney)
     storey.upgrade()
     storey.buy_new_cards()
